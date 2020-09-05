17b6f85f88c646904ee066988d6639ec54ba9eeb
Titas <titas.sarker1234@gmail.com>
2020-09-06 01:28:27 +0600

    some new file added

--- Madman/blog/views.py
+++ Madman/blog/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render,HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, HttpResponse
 
 def home(request):
-    return HttpResponse("this is home")
+    return render(request,'index.html')
+def blog(request):
+    return render(request,'blog.html')
+def contact(request):
+    return render(request,'contact.html')
+def search(request):
+    return render(request,'search.html')
+# def talkingPicture(request):
+#     return render(request,'index.html')
+def talkingPicture(request, slug):
+    return HttpResponse(f"this is talkingPicture {slug}")
