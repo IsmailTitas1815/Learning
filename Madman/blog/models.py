2a92b7e034c99d64c98d268980d4dd6554e10f7d
Titas <titas.sarker1234@gmail.com>
2020-09-11 18:21:02 +0600

    pagenation and contact page design

--- Madman/blog/models.py
+++ Madman/blog/models.py
@@ -7,8 +7,22 @@ class Blog(models.Model):
     sno = models.AutoField(primary_key=True)
     title = models.CharField(max_length=50)
     content = models.TextField()
+    short_desc = models.CharField(max_length=250, default="")
     slug = models.CharField(max_length=100)
     time = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
+
+
+class Contact(models.Model):
+    sno = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=40)
+    phone = models.CharField(max_length=11)
+    desc = models.TextField()
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
--- Madman/blog/views.py
+++ Madman/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
-from blog.models import Blog
+from blog.models import Blog, Contact
+import math
 
 
 def home(request):
@@ -7,12 +8,39 @@ def home(request):
 
 
 def blog(request):
+    no_of_posts = 3
+    page = request.GET.get('page')
+    if page is None:
+        page = 1
+    else:
+        page  = int(page)
+    # print(page)
+    # print(request.GET.get('pageno'))
     blogs = Blog.objects.all()
-    context = {"blogs": blogs}
+    length = len(blogs)
+    blogs = blogs[(page-1)*no_of_posts: page*no_of_posts]
+    if page>1:
+        prev = page-1
+    else:
+        prev = None
+    if page< math.ceil(length/no_of_posts):
+        nxt = page+1
+    else:
+        nxt = None
+    context = {"blogs": blogs, "prev":prev, "nxt":nxt}
     return render(request, 'blog.html', context)
 
 
 def contact(request):
+    if request.method == "POST":
+
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        desc = request.POST.get("desc")
+        instance = Contact(name=name,email=email,phone=phone,desc=desc)
+        instance.save()
+
     return render(request, 'contact.html')
 
 
