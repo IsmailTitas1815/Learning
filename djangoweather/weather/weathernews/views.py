026763040de35e9cc5af1af584d82469af5174d0
Titas <titas.sarker1234@gmail.com>
2020-09-30 00:00:53 +0600

    ok

--- djangoweather/weather/weathernews/views.py
+++ djangoweather/weather/weathernews/views.py
@@ -1,16 +1,33 @@
 import requests
 from django.shortcuts import render
+from .models import City
+from .forms import CityForm
+
 
 def home(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=d329748d0717cb9758b388d98a841492'
-    city = 'Dhaka'
-    r = requests.get(url.format(city)).json()
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=d329748d0717cb9758b388d98a841492'
+    
+    if request.method == "POST":
+        pass
 
-    city_weather = {
-        'city': city,
-        'temperature': r['main']['temp'],
-        'description': r['weather'][0]['description'],
-        'icon': r['weather'][0]['icon'],
-    }
-    context = {'city_weather': city_weather}
+    form = CityForm()
+
+    cities = City.objects.all()
+
+    weather_data = []
+
+    for city in cities:
+
+        r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city.name,
+            'temperature': r['main']['temp'],
+            'description': r['weather'][0]['description'],
+            'icon': r['weather'][0]['icon'],
+        }
+
+        weather_data.append(city_weather)
+    context = { 'weather_data': weather_data ,'form': form }
+
     return render(request,'weather.html',context)
