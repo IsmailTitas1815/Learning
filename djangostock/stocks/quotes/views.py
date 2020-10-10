650191d4f87786d19c9f4ffc94e38045155997b2
Titas <titas.sarker1234@gmail.com>
2020-10-10 21:21:15 +0600

    ok

--- djangostock/stocks/quotes/views.py
+++ djangostock/stocks/quotes/views.py
@@ -2,6 +2,20 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
+    import requests
+    import json
+    api_request = requests.get("https://cloud.iexapis.com/stable/")
+    api = json.loads(api_request.content)
+
+
     return render (request,"home.html")
+
+
+
+
+
 def about(request):
     return render (request,"about.html")
+
+
+# pk_3fa32ca83b874205a6992451c6b94e98
