a1753d6e85b0491c403f6513bcea47340de41552
Titas <titas.sarker1234@gmail.com>
2020-10-11 04:07:48 +0600

    o

--- MyE_commerce/mac/shop/urls.py
+++ MyE_commerce/mac/shop/urls.py
@@ -3,5 +3,11 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.index, name='ShopHome')
+    path("", views.index, name='ShopHome'),
+    path("about/", views.about, name='about'),
+    path("contact/", views.contact, name='contactUs'),
+    path("search/", views.search, name='search'),
+    path("tracker/", views.tracker, name='tracker'),
+    path("prodectview/", views.product, name='prodectview'),
+    path("checkout/", views.checkout, name='checkout'),
 ]
--- MyE_commerce/mac/shop/views.py
+++ MyE_commerce/mac/shop/views.py
@@ -3,4 +3,16 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Index Shop")
+    return render(request, 'shop/index.html')
+def about(request):
+    return render(request, 'shop/about.html')
+def search(request):
+    return render(request, 'shop/search.html')
+def contact(request):
+    return render(request, 'shop/contact.html')
+def tracker(request):
+    return render(request, 'shop/tracker.html')
+def product(request):
+    return render(request, 'shop/product.html')
+def checkout(request):
+    return render(request, 'shop/checkout.html')
