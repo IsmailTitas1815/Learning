07f77e63bb49064bf5392fa6ac24ac2336712288
Titas <titas.sarker1234@gmail.com>
2020-10-29 23:47:16 +0600

    fgh

--- JavaScript/js/tut3.js
+++ JavaScript/js/tut3.js
@@ -1,17 +1,32 @@
-console.log('tut3');
+// console.log('tut3');
 var name = `titas`;
 var channel1;
 channel1 = "topu";
-console.log(name)
+// console.log(name)
 
-{
-//indent
-let name = "ismail";
-console.log(name);
+// {
+// //indent
+// let name = "ismail";
+// console.log(name);
 
-}
-console.log(name)
+// }
+// console.log(name)
 
-function name(params) {
+// function name(params) {
     
-}
+// }
+
+let bool = String(true);
+
+// console.log(typeof(bool))
+
+let arr = [2,3,4,5,6,6];
+// console.log(arr.length);
+console.log(arr,typeof(arr))
+
+// let i = new Date()
+// console.log(i.toString())
+
+let str = parseFloat('454534.09887')
+console.log(str.toFixed(3))
+
