de4904070a41b149ec7fd0cfca74da7a51ad3342
Titas <titas.sarker1234@gmail.com>
2020-10-31 00:28:50 +0600

    vbc

--- JavaScript/js/tut3.js
+++ JavaScript/js/tut3.js
@@ -10,7 +10,7 @@
 //         break
 //     case 2:
 //         day = "tuesday";
-//         break   
+//         break
 //     case 3:
 //         day = "wednesday";
 //         break
@@ -22,9 +22,9 @@
 //         break
 //     case 6:
 //         day = "saturday";
-//         break   
+//         break
 //     default:
-//         day = "nothing"      
+//         day = "nothing"
 
 // }
 
@@ -39,7 +39,6 @@
 
 // console.log(name)
 
-
 // let random = Math.floor(Math.random()*10 )+1
 // console.log(random)
 
@@ -50,20 +49,53 @@
 
 // const a = 5;
 
-// (a==5) 
+// (a==5)
 // ?console.log("ok")
-// :console.log("not ok") 
+// :console.log("not ok")
 
-setTimeout(aa,3000,'hi');
-setTimeout(aaa,3000,'hi');
-setTimeout(aaaa,3000,'hi');
+// setTimeout(aa,3000,'hi');
+// setTimeout(aaa,3000,'hi');
+// setTimeout(aaaa,3000,'hi');
 
-function aa(x){
-    console.log(x);
-}
-function aaa(x){
-    console.log(x);
-}
-function aaaa(x){
-    console.log(x);
-}
+// function aa(x){
+//     console.log(x);
+// }
+// function aaa(x){
+//     console.log(x);
+// }
+// function aaaa(x){
+//     console.log(x);
+// }
+
+// let arr= [1,2,34,5,7,5,4,32,1];
+// let arr2= [1,2,34,5,7,5,4,32,1];
+
+// console.log(arr);
+// arr[0] = 44;
+// console.log(arr);
+// arr.unshift(33);
+// console.log(arr);
+// arr.shift()
+// console.log(arr);
+// arr.splice(2,2)
+// console.log(arr);
+// arr = arr.concat(arr2);
+// console.log(arr);
+
+// let age = 33;
+
+// if (age == "33") {
+//   console.log("ok,33");
+// }
+// if (age !== "33") {
+//   console.log("okkk,33");
+// } else {
+//   console.log("no ok");
+// }
+
+
+
+// alert("fuck");
+// a = prompt("what the fuck");
+a = confirm("sure?");
+console.log(a)
