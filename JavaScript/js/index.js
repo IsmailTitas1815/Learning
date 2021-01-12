b72cd31545026a2a1f3c7ca65aa88eaf7044b8e1
Titas <titas.sarker1234@gmail.com>
2021-01-12 19:15:29 +0600

    javascript

--- JavaScript/js/index.js
+++ JavaScript/js/index.js
@@ -1,6 +1,50 @@
-// console.time("my code");
-console.log('Hello');
-console.table({titas: 'ok',fine: 98});
-// console.warn("this is warning");
-// console.timeEnd("my code");
-console.assert(777<200,"age more than 200 is not possible")
+// let names = ['titas','topu','ismail','hasan','sarker'];
+// console.log(names);
+// names = names.sort();
+
+// names.forEach(function (name) {
+//     console.log(name);
+// })   
+
+
+
+// function operations(a,b,callback) {
+
+//  let c = a+b;
+
+//  callback(c);
+    
+// }
+
+// function plus5(n) {
+//     console.log(n+5);
+// }
+// function minus5(n) {
+//     console.log(n-5);
+// }
+
+// operations(10,5,function (cn) {
+//     console.log(cn);
+// });
+
+
+let person = [
+{
+    name : 'titas',
+    age  : 22,
+    address : 'ashulia',
+    id : [181,15,1815],
+},
+{
+    name : 'ti',
+    age  : 2,
+    address : 'a',
+    id : [18],
+  }
+
+];
+
+person.forEach(function(people) {
+    
+    
+});
