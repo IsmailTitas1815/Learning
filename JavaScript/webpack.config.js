0a3bb96e46c175cf7360db5bc71e2538c085e4f3
Titas <titas.sarker1234@gmail.com>
2021-01-21 21:36:48 +0600

    finally hoiche

--- JavaScript/webpack.config.js
+++ JavaScript/webpack.config.js
@@ -1,22 +1,19 @@
 const path = require('path');
-
-const config = {
-    entry: './src/index.js',
+module.exports = {
+    entry: { main: './src/index.js' },
     output: {
-        path: path.resolve(__dirname,'dist'),
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
     },
-
     module: {
         rules: [
             {
-                test: /\.js$/,
-                use:{
-                    loader:'babel-loader'
+                test: "/\.js$/",
+                exclude: "/node_modules/",
+                use: {
+                    loader: "babel-loader"
                 }
             }
         ]
     }
 }
-
-module.exports = config;
