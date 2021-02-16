443141b00e03f093d787c108515b563ccca1f44a
Titas <titas.sarker1234@gmail.com>
2021-02-16 18:30:57 +0600

    commit

---
+++ JavaScript/component.js
@@ -0,0 +1,17 @@
+class something{
+
+    a = 45;
+    tr = "titas";
+    obj = {
+        name : "ismail",
+        age:23,
+    }
+    arr= [1,2,3,4,5]
+    
+    hello = ()=>{
+        console.log("Hello world");
+    }
+
+};
+
+export default something;
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,50 @@
-let ft= require('date-format');
-let time = ft('dd/MM/yyyy',new Date())
-console.log(time); 
+// import something from './component.js'; 
+
+// let obj1 = new something();
+// console.log(obj1.hello());
+
+// console.log("before error");
+// try{
+//     throw "too cheap";
+// }catch(err){
+//     console.log(err);
+// }finally{
+
+//     console.log("I'm inside finally");
+// }
+
+//regular expression
+// let re;
+// re = /H*llo/;
+
+// let str  = "Hello";
+
+// let fi;
+// fi = re.exec(str);
+// console.log(fi);
+
+
+var xmlhttp = new XMLHttpRequest();
+xmlhttp.onreadystatechange = function(){
+    if (this.readyState == 4 && this.status == 200){
+        // console.log(this.responseText);
+        var data = this.responseText;
+        jsonData(data);
+    }
+};
+
+xmlhttp.open('GET',"data.json",true);
+xmlhttp.send();
+
+function jsonData(data){
+    // console.log(data);
+    var js_obj = JSON.parse(data);
+    // console.log(js_obj);
+    for (i in js_obj.person){
+        var person = js_obj.person;
+        for (j in person[i])
+        {
+            console.log(person[i][j]);
+        }
+    }
+};
