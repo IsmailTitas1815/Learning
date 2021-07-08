dbdd11eb3ed7b71bd27ea2c28a73f15e19127500
IsmailTitas1815 <titas.sarker1234@gmail.com>
2021-07-09 02:44:52 +0600

    ok

--- Bohubrihi/Django/My_Social_Project/App_Login/forms.py
+++ Bohubrihi/Django/My_Social_Project/App_Login/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from App_Login.model import UserProfile
+from App_Login.models import UserProfile
 
 class CreateNewUser(UserCreationForm):
     email = forms.EmailField(required=True, label="",widget=forms.TextInput(attrs={'placeholder':"Email"}))
@@ -10,6 +10,15 @@ class CreateNewUser(UserCreationForm):
 
     password1 = forms.CharField(required=True, label="",widget=forms.PasswordInput(attrs={'placeholder':"Password"}))
     password2 = forms.CharField(required=True, label="",widget=forms.PasswordInput(attrs={'placeholder':"Confirm Password"}))
+
     class Meta:
         model = User
         fields = ('email','username','password1',"password2")
+    
+
+class EditProfile(forms.ModelForm):
+    dob = forms.DateField(widget=forms.TextInput(attrs={'type':'date'}))
+    class Meta:
+        model= UserProfile()
+        fields = "__all__"
+        exclude = ('user',)
--- Bohubrihi/Django/My_Social_Project/App_Login/views.py
+++ Bohubrihi/Django/My_Social_Project/App_Login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
-from App_Login.forms import CreateNewUser
+from App_Login.forms import CreateNewUser, EditProfile
 from django.contrib.auth import authenticate,login,logout
 from django.urls import reverse, reverse_lazy
 from App_Login.models import UserProfile
@@ -18,9 +18,10 @@ def signUp(request):
             user = form.save()
             registered = True
             user_profile = UserProfile(user = user)
+            user_profile.save()
             return HttpResponseRedirect(reverse('App_Login:login'))
             
-    return render(request,'App_Login/sign_up.html',context={'title':'Sign Up . Instagram','form':form})
+    return render(request,'App_Login/sign_up.html',context={'title':'Sign Up . Instagram','form':form,"registered":registered})
 
 
 def login_page(request):
@@ -33,11 +34,30 @@ def login_page(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request,user)
-                pass
+                return HttpResponseRedirect(reverse('App_Posts:home'))
 
 
     return render(request,'App_Login/login.html',context={'title':'Login','form':form})
 
 @login_required
 def edit_profile(request):
-    return render(request,'App_Login/profile.html',context={})
+    current_user = UserProfile.objects.get(user=request.user)
+    form = EditProfile(instance=current_user)
+    if request.method=="POST":
+        form = EditProfile(request.POST, request.FILES, instance = current_user)
+        if form.is_valid():
+            form.save(commit=True)
+            form = EditProfile(instance=current_user)
+
+    return render(request,'App_Login/profile.html',context={'form':form,'title':"Edit Profile. Social"})
+
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('App_Login:login'))
+
+
+@login_required
+def profile(request):
+    return render(request,'App_Login/user.html',context={'title':"Profile"})
